7e45eb28237d44b78183308352a474fd07d30a96
Klesley Silva de Oliveira <klesley.oliveira@hotmail.com>
2026-05-12 15:02:42 -0300

    GET all clients - pageable

--- src/main/java/com/devsuperior/desafio3/services/ClientService.java
+++ src/main/java/com/devsuperior/desafio3/services/ClientService.java
@@ -1,13 +1,14 @@
 package com.devsuperior.desafio3.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.desafio3.dto.ClientDTO;
 import com.devsuperior.desafio3.entities.Client;
 import com.devsuperior.desafio3.repositories.ClientRepository;
-
-import jakarta.transaction.Transactional;
 
 @Service
 public class ClientService {
@@ -25,16 +26,18 @@ public class ClientService {
 		return new ClientDTO(client);
 	}
 
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Client client = repository.findById(id).get();
 		return new ClientDTO(client);
 	}
 
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		return repository.findAll(pageable).map(x -> new ClientDTO(x));
+	}
+
 	/*
-	 * public ResponseEntity<Pageable<ClientDTO>> findAll(){
-	 * 
-	 * }
-	 * 
 	 * public ResponseEntity<ClientDTO> update(Long id, ClientDTO dto) {
 	 * 
 	 * }
